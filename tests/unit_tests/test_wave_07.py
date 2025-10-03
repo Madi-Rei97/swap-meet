ca76561dea0ca6db2497f1a744f5296fb7ca8f4e
Priscilla Andresen <pgdamorim@gmail.com>
2025-10-03 10:30:35 -0700

    test for swap_by_newest

--- tests/unit_tests/test_wave_07.py
+++ tests/unit_tests/test_wave_07.py
@@ -5,3 +5,32 @@ from swap_meet.clothing import Clothing
 from swap_meet.decor import Decor
 from swap_meet.electronics import Electronics
 
+#@pytest.mark.skip
+def test_swap_best_by_category():
+    # Arrange
+    # me
+    item_a = Decor(age=5)
+    item_b = Electronics(age=4)
+    item_c = Decor(age=1)
+    tai = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+    # them
+    item_d = Clothing(age=0)
+    item_e = Decor(age=2)
+    item_f = Clothing(age=5)
+    jesse = Vendor(
+        inventory=[item_d, item_e, item_f]
+    )
+
+    # Act
+    result = tai.swap_by_newest(
+        other_vendor=jesse,
+    )
+
+    assert result
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
+    assert item_c in jesse.inventory
+    assert item_d in tai.inventory
